ce2d59b4023786853f892f5b52cd605811da9d50
Manish_Syal <manishsyal77@gmail.com>
2024-06-02 18:27:00 +0530

    Created middleware for errorHandling

--- backend/controller/messageController.js
+++ backend/controller/messageController.js
@@ -1,16 +1,18 @@
+import { catchAsyncErrors } from "../middlewares/catchAsyncErrors.js";
 import { Message } from "../models/messageSchema.js";
-
-export const sendmessage = async (req, res, next) => {
+import ErrorHandler from "../middlewares/errorMiddleware.js";
+export const sendmessage = catchAsyncErrors(async (req, res, next) => {
   const { firstName, lastName, email, phone, message } = req.body;
   if (!firstName || !lastName || !email || !phone || !message) {
-    return res.status(400).json({
-      success: false,
-      message: "Please Fill Full Form",
-    });
+    return next(new ErrorHandler("Please Fill Full Form", 400));
+    // return res.status(400).json({
+    //   success: false,
+    //   message: "Please Fill Full Form",
+    // });
   }
   await Message.create({ firstName, lastName, email, phone, message });
   res.status(200).json({
     success: true,
     message: "Message send succefully",
   });
-};
+});
