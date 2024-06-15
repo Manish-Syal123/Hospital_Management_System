77e2061c01a57b380a6e1693c99b04fd21ae0f26
Manish_Syal <manishsyal77@gmail.com>
2024-06-15 18:33:24 +0530

    Designed Cards to showcase registered doctors information

--- frontend/src/pages/AboutUs.jsx
+++ frontend/src/pages/AboutUs.jsx
@@ -1,27 +1,54 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "../Styles/about.css"; // Import the CSS file
 import "../Styles/aboutHero.css";
 import Doctors from "../components/Doctors";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { Context } from "../main";
+import { Navigate } from "react-router-dom";
+
 const AboutUs = () => {
+  const [doctors, setDoctors] = useState([]);
+  const { isAuthenticated } = useContext(Context);
+
+  const fetchDoctors = async () => {
+    try {
+      const { data } = await axios.get(
+        "http://localhost:4000/api/v1/user/doctors",
+        { withCredentials: true }
+      );
+      setDoctors(data.doctors);
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchDoctors();
+  }, []);
+  if (!isAuthenticated) {
+    return <Navigate to={"/login"} />;
+  }
+
   return (
     <>
       <section className="hero-section">
         <div className="container1">
           <div className="content">
             <h1 className="hero-title">
-              Understand User Flow.
+              Our Registered Doctors.
               <span className="hero-subtitle"> Increase Conversion. </span>
             </h1>
             <p className="hero-description">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt
-              illo tenetur fuga ducimus numquam ea!
+              We have the best Hired Doctors arround the world, with higher
+              eduction in ther department.
             </p>
             <div className="button-group">
               <a className="button primary-button" href="#">
-                Get Started
+                Check below Our 👉
               </a>
               <a className="button secondary-button" href="#">
-                Learn More
+                Registered Doctor Information 👇
               </a>
             </div>
           </div>
@@ -31,55 +58,45 @@ const AboutUs = () => {
       <section className="career-path-section">
         <div className="container">
           <div className="grid">
-            <div className="text-content">
-              <h2 className="title">Find your career path</h2>
-              <p className="description">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut
-                vero aliquid sint distinctio iure ipsum cupiditate? Quis, odit
-                assumenda? Deleniti quasi inventore, libero reiciendis minima
-                aliquid tempora. Obcaecati, autem.
-              </p>
-              <a href="#" className="cta-button">
-                Get Started Today
-              </a>
-            </div>
-
-            <div className="icon-grid">
-              {Array(3)
-                .fill("")
-                .map((_, index) => (
-                  <a key={index} className="icon-card" href="#">
-                    <span className="icon">
-                      <svg
-                        className="icon-svg"
-                        fill="none"
-                        stroke="currentColor"
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
-                        <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path>
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-                        ></path>
-                      </svg>
+            <div className="doctor-grid">
+              {doctors.map((element, index) => (
+                <a key={index} className="doctor-card">
+                  <div className="doctor-image">
+                    <img
+                      src={element.docAvatar && element.docAvatar.url}
+                      alt="doctor avatar"
+                    />
+                  </div>
+                  <h2 className="doctor-title">
+                    Email:{" "}
+                    <span className="doctor-description">{element.email}</span>
+                  </h2>
+                  <h2 className="doctor-title">
+                    Phone:{" "}
+                    <span className="doctor-description">{element.phone}</span>
+                  </h2>
+                  <h2 className="doctor-title">
+                    DOB:
+                    <span className="doctor-description">
+                      {element.dob.substring(0, 10)}
                     </span>
-                    <h2 className="icon-title">Accountant</h2>
-                    <p className="icon-description">
-                      Lorem ipsum dolor sit amet consectetur.
-                    </p>
-                  </a>
-                ))}
+                  </h2>
+                  <h2 className="doctor-title">
+                    Department:
+                    <span className="doctor-description">
+                      {element.doctorDepartment}
+                    </span>
+                  </h2>
+                  <h2 className="doctor-title">
+                    Gender:
+                    <span className="doctor-description">{element.gender}</span>
+                  </h2>
+                </a>
+              ))}
             </div>
           </div>
         </div>
       </section>
-
-      {/* Available Doctors Information */}
-      <Doctors />
     </>
   );
 };
