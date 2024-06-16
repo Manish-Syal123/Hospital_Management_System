f8f5e62cd3cd3a1fcd692abeaea46a2d6f91bc3b
Manish_Syal <manishsyal77@gmail.com>
2024-06-16 16:24:39 +0530

    Designed the user appointment page

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -1,13 +1,13 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-
+import "../Styles/appointment.css";
 const MyAppointments = () => {
   const [userAppointments, setUserAppointments] = useState([]);
 
   useEffect(() => {
     BookedAppointments();
     console.log(userAppointments);
-  }, []);
+  }, [userAppointments]);
   const BookedAppointments = async () => {
     try {
       const result = await axios.get(
@@ -24,12 +24,67 @@ const MyAppointments = () => {
     }
   };
   return (
-    <div>
-      <h2>Your Appointments</h2>
-      <div>
-        {userAppointments.map((items, index) => (
-          <h2 key={index}>{items.firstName}</h2>
-        ))}
+    <div className="maincontainer">
+      <div className="banner">
+        <h5>My Appointments</h5>
+        <table>
+          <thead>
+            <tr>
+              <th>Patient</th>
+              <th>Date</th>
+              <th>Doctor</th>
+              <th>Department</th>
+              <th>Status</th>
+              <th>Visited</th>
+            </tr>
+          </thead>
+          <tbody>
+            {userAppointments && userAppointments.length > 0 ? (
+              userAppointments.map((appointment) => (
+                <tr key={appointment._id}>
+                  <td data-label="Patient">{`${appointment.firstName} ${appointment.lastName}`}</td>
+                  <td data-label="Date">
+                    {appointment.appointment_date.substring(0, 16)}
+                  </td>
+                  <td data-label="Doctor">{`${appointment.doctor.firstName} ${appointment.doctor.lastName}`}</td>
+                  <td data-label="Department">{appointment.department}</td>
+                  <td data-label="Status">
+                    <select
+                      className={
+                        appointment.status === "Pending"
+                          ? "value-pending"
+                          : appointment.status === "Accepted"
+                          ? "value-accepted"
+                          : "value-rejected"
+                      }
+                      value={appointment.status}
+                      disabled
+                    >
+                      <option value="Pending" className="value-pending">
+                        Pending
+                      </option>
+                      <option value="Accepted" className="value-accepted">
+                        Accepted
+                      </option>
+                      <option value="Rejected" className="value-rejected">
+                        Rejected
+                      </option>
+                    </select>
+                  </td>
+                  <td data-label="Visited">
+                    <button>Cancel</button>
+                  </td>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <td colSpan="6">
+                  <h1>No Appointments Found!</h1>
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </table>
       </div>
     </div>
   );
